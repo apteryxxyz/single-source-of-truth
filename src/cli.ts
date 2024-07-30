c74c5dd267a391354e14cb311a7791cc09a6ba6e
Apteryx <apteryx.xyz@gmail.com>
2024-07-30 20:16:59 +1200

    Add `--on-success` cli option

--- src/cli.ts
+++ src/cli.ts
@@ -1,37 +1,49 @@
 #!/usr/bin/env node
 
+import { exec } from 'node:child_process';
+import { watchFile } from 'node:fs';
 import { resolve } from 'node:path';
 import { pathToFileURL } from 'node:url';
-import logger from './logger';
 import 'tsx';
 import { generate } from './generate';
+import logger from './logger';
 
-const [input] = process.argv.slice(2);
+const [input, ...args] = process.argv.slice(2);
 if (!input) {
   logger.error('Missing input file argument');
   process.exit(1);
 }
 
-const url = pathToFileURL(resolve(input));
+const doWatch = args.indexOf('--watch') !== -1;
+const onSuccessIndex = args.indexOf('--on-success');
+const onSuccess = onSuccessIndex !== -1 ? args[onSuccessIndex + 1] : null;
 
 async function run() {
+  const url = pathToFileURL(resolve(input!));
   url.searchParams.set('t', Date.now().toString());
+
   const source = await import(url.toString()) //
     .then((m) => m.default);
-  if (source) {
-    await generate(source);
-  } else {
-    logger.error('Missing SourceOfTruth export');
+  if (!source) {
+    logger.error('Missing default export');
     process.exit(1);
+  }
+
+  await generate(source);
+  if (onSuccess) {
+    exec(onSuccess, (error, stdout, stderr) => {
+      if (error)
+        logger.error(`Error executing on success command: ${error.message}`);
+      else if (stderr) console.log(stderr);
+      else console.log(stdout);
+    });
   }
 }
 
 logger.info('Generating...');
 await run();
 
-import { watchFile } from 'node:fs';
-const watch = process.argv.includes('--watch');
-if (watch) {
+if (doWatch) {
   logger.info('Watching...');
   watchFile(input, async () => {
     logger.info('Regenerating...');
